8108b11d3790909901871df47f792814fe4cf629
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2021-02-05 06:56:12 -0800

    Added assigments from Ubuntu

---
+++ Week-1/a.sh
@@ -0,0 +1,6 @@
+who | cut -d " " -f1 | uniq
+who | cut -d " " -f1 | uniq | wc -l
+
+# OUTPUT:
+# kinjal
+# 1
---
+++ Week-1/g.sh
@@ -0,0 +1,4 @@
+w | tr -s " " | cut -d" " -f1,5 | tail -n+3
+
+# OUTPUT:
+# kinjal ?xdm?
